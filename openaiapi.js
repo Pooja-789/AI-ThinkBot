6d2cf0e652ef84c489554f7548742e08900cb353
Vivek Gaddipati <vgaddipati@opteamix.com>
2025-01-06 15:51:45 +0530

    summarizing a webpage

--- openaiapi.js
+++ openaiapi.js
@@ -2,7 +2,11 @@ import OpenAI from 'openai';
 const client = new OpenAI();
 
 const response = await client.chat.completions.create({
-    messages: [{ role: 'developer', content: 'get the content of this html string and summarise it : <html><body><div>Vivek is a great programmer</div></body></html>' }],
+    messages: [
+        { role: 'developer', content: `https://www.nrdc.org/stories/global-warming-101` },
+        { role: 'developer', content: 'Provide the body of the HTML web page' },
+        { role: 'user', content: 'Summarize the content and provide me a bulleted text' },
+    ],
     model: 'gpt-4o-mini'
 });
 
