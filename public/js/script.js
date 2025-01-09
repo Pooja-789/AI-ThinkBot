6a6563206008aafad5750d7f1eb2e686ea6170fb
Pooja Ananda <Pananda@opteamix.com>
2025-01-09 17:24:51 +0530

    Added menu o UI changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const askButton = document.getElementById('askButton');
+    const askButtonText = document.getElementById('askButtonText');
+    const askButtonURL = document.getElementById('askButtonURL');
     const textInput = document.getElementById('textInput');
     const urlInput = document.getElementById('urlInput');
-    const answerDiv = document.getElementById('answerDiv');
-    const copyButton = document.getElementById('copyButton');
+    const answerDivText = document.getElementById('answerDivText');
+    const answerDivURL = document.getElementById('answerDivURL');
+    const copyButtonText = document.getElementById('copyButtonText');
+    const copyButtonURL = document.getElementById('copyButtonURL');
     const progressBarContainer = document.getElementById('progressBarContainer');
     const progressBar = document.getElementById('progressBar');
+    const hamburgerIcon = document.getElementById('menuDisplay');
+    const menuDiv = document.getElementById('menu');
+    const textContentMenu = document.getElementById('textContentMenu');
+    const webpageContentMenu = document.getElementById('webpageContentMenu');
+    const textContent = document.getElementById('textContent');
+    const webpageContent = document.getElementById('webpageContent');
+
+    textContent.style.display = 'none';
+    webpageContent.style.display = 'none';
+    textContentMenu.style.color = 'grey'
 
     function showProgressBar() {
         progressBarContainer.style.display = 'block';
@@ -40,71 +53,87 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             textInput.style.height = `${textInput.scrollHeight}px`;
         }
-        urlInput.disabled = textInput.value.trim().length > 0;
     });
 
-    urlInput.addEventListener('input', function () {
-        textInput.disabled = urlInput.value.trim().length > 0;
-    });
-
-    askButton.addEventListener('click', function () {
-        let prompt = textInput.value.trim(); // First, prioritize textInput
+    askButtonText.addEventListener('click', function () {
+        let prompt = textInput.value.trim(); // Text input from Text Summarizer page
 
         if (prompt) {
             const urlPattern = /https?:\/\/[^\s]+/;
             if (urlPattern.test(prompt)) {
-                answerDiv.textContent = 'Please provide valid text, not a URL.';
+                answerDivText.textContent = 'Please provide valid text, not a URL.';
                 return;
             }
+            const interval = showProgressBar();
+            fetch('/api/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ prompt: prompt })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    let message = data.message;
+                    if (message) {
+                        const bulletPattern = /- \*\*(.*)/g;
+                        message = message.replace(bulletPattern, '<li>$1</li>');
+                        message = `<ul>${message}</ul>`;
+                    }
+                    answerDivText.innerHTML = message;
+                    hideProgressBar(interval);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    answerDivText.textContent = 'Failed to load response. ' + error.message;
+                    hideProgressBar(interval);
+                });
         } else {
-            prompt = urlInput.value.trim();
-            if (!prompt) {
-                answerDiv.textContent = 'Please provide either a text or a URL.';
-                return;
-            }
-            const urlPattern = /https?:\/\/[^\s]+/;
-            if (!urlPattern.test(prompt)) {
-                answerDiv.textContent = 'Please provide a valid URL.';
-                return;
-            }
+            answerDivText.textContent = 'Please provide some text to summarize.';
         }
-        const interval = showProgressBar();
-
-        // Proceed with the fetch request if input is valid
-        fetch('/api/query', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ prompt: prompt })
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Received data:', data);
-                let message = data.message;
-                if (message) {
-                    const bulletPattern = /- \*\*(.*)/g;
-                    message = message.replace(bulletPattern, '<li>$1</li>');
-                    message = `<ul>${message}</ul>`;
-                }
-                answerDiv.innerHTML = message;
-                hideProgressBar(interval);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                answerDiv.textContent = 'Failed to load response. ' + error.message;
-                hideProgressBar(interval);
-            })
     });
 
-    copyButton.addEventListener('click', function () {
-        if (answerDiv.textContent) {
-            navigator.clipboard.writeText(answerDiv.textContent)
+    askButtonURL.addEventListener('click', function () {
+        let prompt = urlInput.value.trim(); // URL input from Webpage Summarizer page
+
+        if (prompt) {
+            const urlPattern = /https?:\/\/[^\s]+/;
+            if (!urlPattern.test(prompt)) {
+                answerDivURL.innerHTML = 'Please provide a valid URL.';
+                return;
+            }
+            const interval = showProgressBar();
+            fetch('/api/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ prompt: prompt })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    let message = data.message;
+                    if (message) {
+                        const bulletPattern = /- \*\*(.*)/g;
+                        message = message.replace(bulletPattern, '<li>$1</li>');
+                        message = `<ul>${message}</ul>`;
+                    }
+                    answerDivURL.innerHTML = message;
+                    hideProgressBar(interval);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    answerDivURL.innerHTML = 'Failed to load response. ' + error.message;
+                    hideProgressBar(interval);
+                });
+        } else {
+            answerDivURL.webpageContent = 'Please provide a URL to summarize.';
+        }
+    });
+
+    copyButtonText.addEventListener('click', function () {
+        if (answerDivText.textContent) {
+            navigator.clipboard.writeText(answerDivText.textContent)
                 .then(() => {
                     console.log('Text copied to clipboard');
                 })
@@ -113,4 +142,40 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
         }
     });
+
+    copyButtonURL.addEventListener('click', function () {
+        if (answerDivURL.textContent) {
+            navigator.clipboard.writeText(answerDivURL.textContent)
+                .then(() => {
+                    console.log('URL response copied to clipboard');
+                })
+                .catch(err => {
+                    console.error('Error in copying text: ', err);
+                });
+        }
+    });
+
+    hamburgerIcon.addEventListener('click', function () {
+        if (menuDiv.style.display === 'none' || menuDiv.style.display === '') {
+            menuDiv.style.display = 'block';
+            console.log('Menu shown');
+        } else {
+            menuDiv.style.display = 'none';
+            console.log('Menu hidden');
+        }
+    });
+
+    textContentMenu.addEventListener('click', function () {
+        textContentMenu.style.color = 'blue'
+        webpageContentMenu.style.color = 'grey'
+        textContent.style.display = 'block'
+        webpageContent.style.display = 'none'
+    })
+
+    webpageContentMenu.addEventListener('click', function () {
+        webpageContentMenu.style.color = 'blue'
+        textContentMenu.style.color = 'grey'
+        textContent.style.display = 'none'
+        webpageContent.style.display = 'block'
+    })
 });
